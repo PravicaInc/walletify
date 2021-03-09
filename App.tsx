ffd7aa0c74f7676b8936bb5b7ae73b87e278d485
Mina <minaf65@gmail.com>
2021-03-09 12:21:03 +0200

    Installed crypto libs

--- App.tsx
+++ App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -29,6 +19,7 @@ import {
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
+  console.warn(Buffer);
   return (
     <>
       <StatusBar barStyle="dark-content" />
