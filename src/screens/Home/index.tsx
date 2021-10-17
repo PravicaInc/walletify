8ab866c944cfae7b72f50a78b9487a2ea7fcf483
Mostafa Amr <mostafa.amr137@gmail.com>
2021-10-17 16:41:53 +0200

    safe area view integration

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
 import { styles } from './styles';
 import { MyText } from '../../components/shared/myText';
 import Wise from '../../assets/wise.svg';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const Home: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Wise />
       <MyText type="bigTitle">It Works</MyText>
-    </View>
+    </SafeAreaView>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,8 +3,5 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 64,
-    paddingHorizontal: 28,
-    backgroundColor: 'white',
   },
 });
