162c3ea2a6bc4e104c1a1cc29994abb0e92fee6f
Khaled <khaled@Khaleds-MacBook-Pro.local>
2021-11-09 21:34:43 +0200

    bugfix/create password screen bottom button doesn't get squished into its top element when input errors show

--- src/screens/CreatePassword/styles.ts
+++ src/screens/CreatePassword/styles.ts
@@ -52,7 +52,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '90%',
-    marginTop: 20,
+    marginVertical: 20,
   },
   switchLabelContainer: {
     flexDirection: 'row',
