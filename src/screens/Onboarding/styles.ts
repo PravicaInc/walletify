22648dc026adb11efd77be0d8074b21f21bcc782
Khaled <khaled@Khaleds-MacBook-Pro.local>
2021-11-09 19:19:49 +0200

    bugFix/increase on boarding swipable area

--- src/screens/Onboarding/styles.ts
+++ src/screens/Onboarding/styles.ts
@@ -7,6 +7,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingBottom: '25%',
     paddingHorizontal: PADDING_HORIZONTAL,
   },
   title: {
