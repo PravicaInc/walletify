fca44aaf08f45787a476b2bda899dba7ff633b9a
Mahmoud Aboelenein <mahmoud@pravica.io>
2021-11-18 14:23:25 +0200

    implemented stacks networks global states

--- src/hooks/useNetwork/useNetwork.ts
+++ src/hooks/useNetwork/useNetwork.ts
@@ -1,49 +1,39 @@
 import { StacksMainnet, StacksNetwork, StacksTestnet } from '@stacks/network';
 import { ChainID } from '@stacks/transactions';
-import { useCallback, useContext, useMemo } from 'react';
+import { useMemo } from 'react';
 import {
   DEFAULT_MAINNET_SERVER,
   DEFAULT_TESTNET_SERVER,
-} from '../shared/constants';
-import { AvailableNetworks, defaultNetworks } from '../models/network';
-import { UserPreferenceContext } from '../contexts/UserPreference/userPreferenceContext';
+} from '../../shared/constants';
+import { useAtom } from 'jotai';
+import { selectedNetworkKey, selectedNetwork } from './networkStore';
+import { useAtomValue } from 'jotai/utils';
 
 const useNetwork = () => {
-  const {
-    userPreference: { selectedNetworkKey },
-    setSelectedNetworkKey,
-  } = useContext(UserPreferenceContext);
+  const [currentNetworkKey, setCurrentNetworkKey] = useAtom(selectedNetworkKey);
+  const currentNetwork = useAtomValue(selectedNetwork);
 
-  const selectedNetwork = useMemo(
-    () => defaultNetworks[selectedNetworkKey],
-    [selectedNetworkKey],
-  );
-
-  const activeStacksNetwork: StacksNetwork | undefined = useMemo(() => {
-    if (selectedNetwork?.chainId === ChainID.Testnet) {
+  const currentNetworkInstance: StacksNetwork | undefined = useMemo(() => {
+    if (currentNetwork?.chainId === ChainID.Testnet) {
       const instanceWithBnsUrl = new StacksTestnet({
         url: DEFAULT_TESTNET_SERVER,
       });
-      instanceWithBnsUrl.bnsLookupUrl = selectedNetwork.url;
+      instanceWithBnsUrl.bnsLookupUrl = currentNetwork.url;
       return instanceWithBnsUrl;
-    } else if (selectedNetwork?.chainId === ChainID.Mainnet) {
+    } else if (currentNetwork?.chainId === ChainID.Mainnet) {
       const instanceWithBnsUrl = new StacksMainnet({
         url: DEFAULT_MAINNET_SERVER,
       });
-      instanceWithBnsUrl.bnsLookupUrl = selectedNetwork.url;
+      instanceWithBnsUrl.bnsLookupUrl = currentNetwork.url;
       return instanceWithBnsUrl;
     }
-  }, [selectedNetwork]);
-
-  const setActiveNetwork = useCallback((network: AvailableNetworks) => {
-    setSelectedNetworkKey(network);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [currentNetwork]);
 
   return {
-    activeStacksNetwork,
-    selectedNetwork,
-    setActiveNetwork,
+    currentNetworkKey,
+    setCurrentNetworkKey,
+    currentNetworkInstance,
+    currentNetwork,
   };
 };
 
