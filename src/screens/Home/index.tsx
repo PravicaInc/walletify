15654d6fc5585532d74c894d02679a41279b6e06
Mahmoud Aboelenein <mahmoud@pravica.io>
2021-12-01 22:00:18 +0200

    WIP: Home Screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,25 @@
-import React, { useContext, useEffect } from 'react';
-import { TouchableOpacity, ScrollView } from 'react-native';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { StackActions, useNavigation } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import AccountAvatar from '../../components/shared/AccountAvatar';
-import { useWallet } from '../../hooks/useWallet/useWallet';
-import Wise from '../../assets/wise.svg';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import Header from '../../components/shared/Header';
 import { ThemeContext } from '../../contexts/Theme/theme';
+import { useWallet } from '../../hooks/useWallet/useWallet';
+import Header from '../../components/shared/Header';
 import { RootStackParamList } from '../../navigation/types';
+import HomeTabs from './HomeTabs';
+import AccountBalanceCard from './AccountBalanceCard';
+import Wise from '../../assets/wise.svg';
+import Settings from '../../assets/images/settings/settings.svg';
 import { styles } from './styles';
+import { useAccounts } from '../../hooks/useAccounts/useAccounts';
+import { Typography } from '../../components/shared/Typography';
+import SwitchAccountBottomSheet from '../../components/Accounts/SwitchAccountBottomSheet';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { useAtomValue } from 'jotai/utils';
+import { currentAnchoredAccountBalancesState } from '../../hooks/useAccounts/accountsStore';
+import { withSuspense } from '../../components/shared/WithSuspense';
+import SwitchAccountButton from './SwitchAccountButton';
+import { useStxPriceValue } from '../../hooks/useStxPrice/useStxPrice';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
@@ -17,32 +27,70 @@ const Home = (props: Props) => {
   const {
     theme: { colors },
   } = useContext(ThemeContext);
-
-  const { dispatch } = useNavigation();
-
   const { restoreWallet } = useWallet();
+  const { dispatch } = useNavigation();
+  const { switchAccount } = useAccounts();
+  const bottomSheetModalRef = useRef<BottomSheet>(null);
+  const balances = useAtomValue(currentAnchoredAccountBalancesState);
+  const stxPrice = useStxPriceValue();
+
   const { password, seedPhrase } = props.route.params;
 
   useEffect(() => {
     restoreWallet(seedPhrase, password);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const goToSettings = () => dispatch(StackActions.push('Settings'));
 
+  const handlePressSwitchAccount = useCallback(() => {
+    bottomSheetModalRef.current?.snapToIndex(0);
+  }, []);
+
+  const handleCancelSwitchAccount = useCallback(() => {
+    bottomSheetModalRef.current?.close();
+  }, []);
+
+  const handleSwitchAccount = useCallback((accountIndex: number) => {
+    switchAccount(accountIndex);
+    handleCancelSwitchAccount();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: colors.white }]}>
-      <ScrollView contentContainerStyle={styles.contentContainer}>
+    <View style={[styles.container, { backgroundColor: colors.white }]}>
+      <View style={styles.contentContainer}>
         <Header
-          leftComponent={<Wise />}
+          leftComponent={<Wise width={92} height={36} />}
           rightComponent={
-            <TouchableOpacity onPress={goToSettings}>
-              <AccountAvatar accountIndex={3} diameter={40} hasAura={true} />
+            <TouchableOpacity
+              onPress={goToSettings}
+              activeOpacity={0.5}
+              style={styles.settingsButton}>
+              <Settings />
             </TouchableOpacity>
           }
         />
-      </ScrollView>
-    </SafeAreaView>
+        <Typography
+          type="commonText"
+          style={[styles.pickAccountText, { color: colors.primary40 }]}>
+          Pick an Account
+        </Typography>
+        <SwitchAccountButton
+          handlePressSwitchAccount={handlePressSwitchAccount}
+        />
+        <AccountBalanceCard balance={balances?.stx.balance} price={stxPrice} />
+        <View style={styles.walletOperationsTabs}>
+          <HomeTabs />
+        </View>
+      </View>
+      <SwitchAccountBottomSheet
+        bottomSheetRef={bottomSheetModalRef}
+        onSwitch={handleSwitchAccount}
+        onCancel={handleCancelSwitchAccount}
+      />
+    </View>
   );
 };
 
-export default Home;
+export default withSuspense(Home, <Text>"Loading..."</Text>);
