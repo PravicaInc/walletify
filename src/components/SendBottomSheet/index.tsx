76636b8d08746e06d905042617a4909dfd021b4b
Omar Emad Eldin <omar@pravica.io>
2022-01-07 12:21:49 +0200

    styling fixes

--- src/components/SendBottomSheet/index.tsx
+++ src/components/SendBottomSheet/index.tsx
@@ -21,9 +21,7 @@ import SimpleTextInput from './SimpleTextInput';
 import ScanQrIcon from '../../assets/images/scanQr.svg';
 import GeneralButton from '../shared/GeneralButton';
 import ScanQrBottomSheet from '../ScanQrBottomSheet';
-import { useAtomValue } from 'jotai/utils';
 import { truncateAddress } from '../../shared/addressUtils';
-import { apiClientState } from '../../hooks/apiClients/apiClients';
 import useNetwork from '../../hooks/useNetwork/useNetwork';
 import WarningIcon from '../shared/WarningIcon';
 import { useAccounts } from '../../hooks/useAccounts/useAccounts';
@@ -148,12 +146,12 @@ const SendBottomSheet = React.forwardRef<any, Props>(
 
     useEffect(() => {
       const fetchFees = async () => {
-        const fees = await estimateTransactionFees(
+        const transactionFees = await estimateTransactionFees(
           recipient,
           Number(amount),
           memo,
         );
-        setFees(fees);
+        setFees(transactionFees);
       };
       if (preview) {
         fetchFees();
@@ -170,6 +168,236 @@ const SendBottomSheet = React.forwardRef<any, Props>(
     const truncatedRecipient = truncateAddress(recipient, 11);
     const truncatedSender = truncateAddress(account?.address, 11);
 
+    function RenderCreateTransaction() {
+      return (
+        <>
+          <Header
+            title="Send"
+            leftComponent={
+              <HeaderBack
+                textColor={colors.secondary100}
+                text="Cancel"
+                onPress={dismissBottomSheet}
+              />
+            }
+          />
+          <KeyboardAwareScrollView
+            resetScrollToCoords={{ x: 0, y: 0 }}
+            contentContainerStyle={styles.inputsContainer}
+            scrollEnabled={false}
+            extraHeight={100}
+            extraScrollHeight={100}>
+            <View style={styles.horizontalFill}>
+              <View style={styles.horizontalFill}>
+                <Typography
+                  type="commonText"
+                  style={{ color: colors.primary40 }}>
+                  Select Asset
+                </Typography>
+                {selectedAsset && (
+                  <AccountAsset
+                    item={selectedAsset}
+                    showFullBalance
+                    fullBalance={`~ $${fullBalance}`}
+                  />
+                )}
+              </View>
+              <View style={styles.horizontalFill}>
+                <SimpleTextInput
+                  onChangeText={setAmount}
+                  value={amount}
+                  label="Amount To Send"
+                  placeholder="0.00000000"
+                  keyboardType="decimal-pad"
+                  icon={
+                    <Typography
+                      type="smallTitleR"
+                      style={{ color: colors.primary40 }}>
+                      STX
+                    </Typography>
+                  }
+                  subtext={
+                    <Typography
+                      type="commonText"
+                      style={[
+                        {
+                          color: colors.primary40,
+                        },
+                        styles.alignRight,
+                      ]}>
+                      {`~ $${(+amount * price).toFixed(2)}`}
+                    </Typography>
+                  }
+                  errorMessage={errorMessages.amount}
+                />
+                <SimpleTextInput
+                  onChangeText={setRecipient}
+                  value={recipient}
+                  label="Recipient Address"
+                  placeholder="Enter an address"
+                  maxLength={50}
+                  icon={
+                    <TouchableOpacity onPress={handlePresentQrScan}>
+                      <ScanQrIcon />
+                    </TouchableOpacity>
+                  }
+                  errorMessage={errorMessages.recipient}
+                />
+                <ScanQrBottomSheet
+                  ref={qrScanRef}
+                  setRecipient={setRecipient}
+                />
+                <SimpleTextInput
+                  onChangeText={setMemo}
+                  value={memo}
+                  label="Memo (For exchanges)"
+                  placeholder="Enter a memo"
+                  subtext={
+                    <Typography
+                      type="commonText"
+                      style={{ color: colors.primary40 }}>
+                      If you are sending to an exchange, be sure to include the
+                      memo the exchange provided so the STX is credited to your
+                      account
+                    </Typography>
+                  }
+                />
+              </View>
+            </View>
+            <GeneralButton
+              type="Primary"
+              disabled={!isReadyForPreview}
+              onPress={() => setPreview(true)}>
+              Preview
+            </GeneralButton>
+          </KeyboardAwareScrollView>
+        </>
+      );
+    }
+
+    function RenderPreviewTransaction() {
+      return (
+        <>
+          <Header
+            title="Send"
+            leftComponent={
+              <HeaderBack
+                textColor={colors.secondary100}
+                text="Back"
+                hasChevron
+                onPress={() => setPreview(false)}
+              />
+            }
+          />
+          <View style={styles.inputsContainer}>
+            <View style={styles.horizontalFill}>
+              <View
+                style={
+                  (styles.previewCard,
+                  {
+                    backgroundColor: colors.card,
+                  })
+                }>
+                <Typography
+                  type="commonText"
+                  style={{ color: colors.primary40 }}>
+                  You Will Send
+                </Typography>
+                {selectedAsset && (
+                  <AccountAsset
+                    item={selectedAsset}
+                    showCustomAmount
+                    customAmount={`${amount}`}
+                  />
+                )}
+                <View
+                  style={
+                    (styles.transactionDetails,
+                    {
+                      borderTopColor: colors.primary20,
+                      borderBottomColor: colors.primary20,
+                    })
+                  }>
+                  <View>
+                    <Typography
+                      type="commonText"
+                      style={{ color: colors.primary40 }}>
+                      From
+                    </Typography>
+                    <Typography type="smallTitleR">
+                      {`(${truncatedSender})`}
+                    </Typography>
+                  </View>
+                  <View>
+                    <Typography
+                      type="commonText"
+                      style={{ color: colors.primary40 }}>
+                      To
+                    </Typography>
+                    <Typography type="smallTitleR">
+                      {`(${truncatedRecipient})`}
+                    </Typography>
+                  </View>
+                </View>
+                <Typography
+                  type="commonText"
+                  style={{ color: colors.primary40 }}>
+                  Memo
+                </Typography>
+                <Typography type="commonText">{memo}</Typography>
+              </View>
+              <View style={styles.horizontalFill}>
+                <View style={styles.transactionMetadataItem}>
+                  <Typography
+                    type="commonText"
+                    style={{ color: colors.primary40 }}>
+                    Fees
+                  </Typography>
+                  <Typography
+                    type="commonText"
+                    style={{ color: colors.primary40 }}>
+                    {`${fees} STX`}
+                  </Typography>
+                </View>
+                <View style={styles.transactionMetadataItem}>
+                  <Typography
+                    type="commonText"
+                    style={{ color: colors.primary40 }}>
+                    Network
+                  </Typography>
+                  <Typography
+                    type="commonText"
+                    style={{ color: colors.primary40 }}>
+                    {currentNetwork.name}
+                  </Typography>
+                </View>
+              </View>
+            </View>
+            <View style={(styles.horizontalFill, styles.centerItems)}>
+              <WarningIcon fill={colors.primary60} />
+              <Typography
+                type="commonText"
+                style={
+                  (styles.warningText,
+                  {
+                    color: colors.primary60,
+                  })
+                }>
+                If you confirm this transaction it is not reversible. Make sure
+                all arguments are correct.
+              </Typography>
+              <GeneralButton
+                type="Primary"
+                disabled={isLoading}
+                onPress={handleSendPress}>
+                {isLoading ? 'Sending...' : 'Send'}
+              </GeneralButton>
+            </View>
+          </View>
+        </>
+      );
+    }
+
     return (
       <Portal>
         <BottomSheet
@@ -180,245 +408,7 @@ const SendBottomSheet = React.forwardRef<any, Props>(
           enablePanDownToClose
           backdropComponent={CustomBackdrop}>
           <View style={styles.container}>
-            {preview ? (
-              <>
-                <Header
-                  title="Send"
-                  leftComponent={
-                    <HeaderBack
-                      textColor={colors.secondary100}
-                      text="Back"
-                      hasChevron
-                      onPress={() => setPreview(false)}
-                    />
-                  }
-                />
-                <View style={styles.inputsContainer}>
-                  <View style={{ width: '100%' }}>
-                    <View
-                      style={{
-                        backgroundColor: colors.card,
-                        borderRadius: 24,
-                        width: '100%',
-                        paddingHorizontal: 21,
-                        paddingVertical: 14,
-                      }}>
-                      <Typography
-                        type="commonText"
-                        style={{ color: colors.primary40 }}>
-                        You Will Send
-                      </Typography>
-                      {selectedAsset && (
-                        <AccountAsset
-                          item={selectedAsset}
-                          showCustomAmount
-                          customAmount={`${amount}`}
-                        />
-                      )}
-                      <View
-                        style={{
-                          width: '100%',
-                          borderBottomWidth: 0.5,
-                          borderTopWidth: 0.5,
-                          borderTopColor: colors.primary20,
-                          borderBottomColor: colors.primary20,
-                          flexDirection: 'row',
-                          justifyContent: 'space-between',
-                          paddingVertical: 20,
-                        }}>
-                        <View>
-                          <Typography
-                            type="commonText"
-                            style={{ color: colors.primary40 }}>
-                            From
-                          </Typography>
-                          <Typography type="smallTitleR">
-                            {`(${truncatedSender})`}
-                          </Typography>
-                        </View>
-                        <View>
-                          <Typography
-                            type="commonText"
-                            style={{ color: colors.primary40 }}>
-                            To
-                          </Typography>
-                          <Typography type="smallTitleR">
-                            {`(${truncatedRecipient})`}
-                          </Typography>
-                        </View>
-                      </View>
-                      <Typography
-                        type="commonText"
-                        style={{ color: colors.primary40 }}>
-                        Memo
-                      </Typography>
-                      <Typography type="commonText">{memo}</Typography>
-                    </View>
-                    <View style={{ width: '100%' }}>
-                      <View
-                        style={{
-                          width: '100%',
-                          flexDirection: 'row',
-                          justifyContent: 'space-between',
-                          padding: 21,
-                        }}>
-                        <Typography
-                          type="commonText"
-                          style={{ color: colors.primary40 }}>
-                          Fees
-                        </Typography>
-                        <Typography
-                          type="commonText"
-                          style={{ color: colors.primary40 }}>
-                          {`${fees} STX`}
-                        </Typography>
-                      </View>
-                      <View
-                        style={{
-                          width: '100%',
-                          flexDirection: 'row',
-                          justifyContent: 'space-between',
-                          paddingHorizontal: 21,
-                        }}>
-                        <Typography
-                          type="commonText"
-                          style={{ color: colors.primary40 }}>
-                          Network
-                        </Typography>
-                        <Typography
-                          type="commonText"
-                          style={{ color: colors.primary40 }}>
-                          {currentNetwork.name}
-                        </Typography>
-                      </View>
-                    </View>
-                  </View>
-                  <View style={{ width: '100%', alignItems: 'center' }}>
-                    <WarningIcon fill={colors.primary60} />
-                    <Typography
-                      type="commonText"
-                      style={{
-                        color: colors.primary60,
-                        textAlign: 'center',
-                        padding: 10,
-                        marginBottom: 20,
-                      }}>
-                      If you confirm this transaction it is not reversible. Make
-                      sure all arguments are correct.
-                    </Typography>
-                    <GeneralButton
-                      type="Primary"
-                      disabled={isLoading}
-                      onPress={handleSendPress}>
-                      {isLoading ? 'Sending...' : 'Send'}
-                    </GeneralButton>
-                  </View>
-                </View>
-              </>
-            ) : (
-              <>
-                <Header
-                  title="Send"
-                  leftComponent={
-                    <HeaderBack
-                      textColor={colors.secondary100}
-                      text="Cancel"
-                      onPress={dismissBottomSheet}
-                    />
-                  }
-                />
-                <KeyboardAwareScrollView
-                  resetScrollToCoords={{ x: 0, y: 0 }}
-                  contentContainerStyle={styles.inputsContainer}
-                  scrollEnabled={false}
-                  extraHeight={100}
-                  extraScrollHeight={100}>
-                  <View style={styles.horizontalFill}>
-                    <View style={styles.horizontalFill}>
-                      <Typography
-                        type="commonText"
-                        style={{ color: colors.primary40 }}>
-                        Select Asset
-                      </Typography>
-                      {selectedAsset && (
-                        <AccountAsset
-                          item={selectedAsset}
-                          showFullBalance
-                          fullBalance={`~ $${fullBalance}`}
-                        />
-                      )}
-                    </View>
-                    <View style={styles.horizontalFill}>
-                      <SimpleTextInput
-                        onChangeText={setAmount}
-                        value={amount}
-                        label="Amount To Send"
-                        placeholder="0.00000000"
-                        keyboardType="decimal-pad"
-                        icon={
-                          <Typography
-                            type="smallTitleR"
-                            style={{ color: colors.primary40 }}>
-                            STX
-                          </Typography>
-                        }
-                        subtext={
-                          <Typography
-                            type="commonText"
-                            style={[
-                              {
-                                color: colors.primary40,
-                              },
-                              styles.alignRight,
-                            ]}>
-                            {`~ $${(+amount * price).toFixed(2)}`}
-                          </Typography>
-                        }
-                        errorMessage={errorMessages.amount}
-                      />
-                      <SimpleTextInput
-                        onChangeText={setRecipient}
-                        value={recipient}
-                        label="Recipient Address"
-                        placeholder="Enter an address"
-                        maxLength={50}
-                        icon={
-                          <TouchableOpacity onPress={handlePresentQrScan}>
-                            <ScanQrIcon />
-                          </TouchableOpacity>
-                        }
-                        errorMessage={errorMessages.recipient}
-                      />
-                      <ScanQrBottomSheet
-                        ref={qrScanRef}
-                        setRecipient={setRecipient}
-                      />
-                      <SimpleTextInput
-                        onChangeText={setMemo}
-                        value={memo}
-                        label=" Memo (For exchanges)"
-                        placeholder="Enter a memo"
-                        subtext={
-                          <Typography
-                            type="commonText"
-                            style={{ color: colors.primary40 }}>
-                            If you are sending to an exchange, be sure to
-                            include the memo the exchange provided so the STX is
-                            credited to your account
-                          </Typography>
-                        }
-                      />
-                    </View>
-                  </View>
-                  <GeneralButton
-                    type="Primary"
-                    disabled={!isReadyForPreview}
-                    onPress={() => setPreview(true)}>
-                    Preview
-                  </GeneralButton>
-                </KeyboardAwareScrollView>
-              </>
-            )}
+            {preview ? RenderPreviewTransaction() : RenderCreateTransaction()}
           </View>
         </BottomSheet>
       </Portal>
