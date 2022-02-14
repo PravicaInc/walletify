7b72e4a989327c73d75c302f42e3d7ce98c1e8cf
Mahmoud Aboelenein <mahmoud@pravica.io>
2022-02-14 21:51:58 +0200

    WISE-19 - added sentry crashlytics Reporting

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Routes } from './src/navigation';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,9 +7,18 @@ import DefaultTheme from './src/themes/defaultTheme';
 import UserPreference from './src/contexts/UserPreference/userPreference';
 import { PortalProvider } from '@gorhom/portal';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import * as Sentry from '@sentry/react-native';
+import Config from 'react-native-config';
 
 export default function App() {
   const [theme, setTheme] = useState<Theme>(DefaultTheme);
+
+  useEffect(() => {
+    Sentry.init({
+      dsn: Config.SENTRY_URL,
+      tracesSampleRate: 1.0,
+    });
+  }, []);
 
   return (
     <UserPreference>
